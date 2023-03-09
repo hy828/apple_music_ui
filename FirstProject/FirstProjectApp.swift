eacc1cfc7f0933ab2329cb9cf2f6ea2536d33e94
lhy <3200300162@zju.edu.cn>
2023-03-09 22:16:34 +0800

    Initial Commit

---
+++ FirstProject/FirstProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirstProjectApp.swift
+//  FirstProject
+//
+//  Created by lhy on 2023/3/9.
+//
+
+import SwiftUI
+
+@main
+struct FirstProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
