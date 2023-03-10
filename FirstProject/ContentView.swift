117cada913dcdab0ed30c9165d47211f0332097a
lhy <3200300162@zju.edu.cn>
2023-03-10 11:43:27 +0800

    Listen Now + Library

--- FirstProject/ContentView.swift
+++ FirstProject/ContentView.swift
@@ -8,14 +8,113 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var selectedButton: NavigationBar = .library
+    
+    enum NavigationBar {
+        case listenNow
+        case browse
+        case radio
+        case library
+        case search
+    }
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        TabView(selection: $selectedButton) {
+            ListenNow
+                .tabItem {
+                    Image(systemName: "play.circle.fill")
+                    Text("Listen Now")
+                }
+            Text("Browse")
+                .tabItem {
+                    Image(systemName: "square.grid.2x2.fill")
+                    Text("Browse")
+                }
+            Text("Radio")
+                .tabItem {
+                    Image(systemName: "dot.radiowaves.left.and.right")
+                    Text("Radio")
+                }
+            Library
+                .tabItem {
+                    Image(systemName: "square.stack.fill")
+                    Text("Library")
+                }
+            Text("Search")
+                .tabItem {
+                    Image(systemName: "magnifyingglass")
+                    Text("Search")
+                }
+        }
+    }
+    
+    var ListenNow: some View {
+        NavigationStack {
+            ScrollView {
+                Text("Top Picks")
+                    .font(.title2)
+                    .bold()
+                    .padding()
+                    .frame(maxWidth: .infinity,
+                           maxHeight: 30,
+                           alignment: .topLeading)
+                    .border(.red)
+                TopPicks
+                RecentlyPlayed
+                RecentlyPlayed
+                RecentlyPlayed
+            }
+            .navigationTitle("Listen Now")
+        }
+    }
+    
+    var TopPicks: some View {
+        ScrollView(.horizontal, showsIndicators: false){
+            HStack {
+                RoundedRectangle(cornerRadius: 15)
+                    .frame(width: 220, height: 270, alignment: .topLeading)
+                RoundedRectangle(cornerRadius: 15)
+                    .frame(width: 220, height: 270, alignment: .topLeading)
+                RoundedRectangle(cornerRadius: 15)
+                    .frame(width: 220, height: 270, alignment: .topLeading)
+            }
         }
         .padding()
+    }
+    
+    var RecentlyPlayed: some View {
+        ScrollView(.horizontal, showsIndicators: false){
+            HStack {
+                RoundedRectangle(cornerRadius: 10)
+                    .frame(width: 180, height: 180, alignment: .topLeading)
+                RoundedRectangle(cornerRadius: 10)
+                    .frame(width: 180, height: 180, alignment: .topLeading)
+                RoundedRectangle(cornerRadius: 10)
+                    .frame(width: 180, height: 180, alignment: .topLeading)
+            }
+        }
+        .padding()
+    }
+    
+    var Library: some View {
+        NavigationStack {
+            VStack {
+                List {
+                    Text("Playlists")
+                    Text("Artists")
+                    Text("Albums")
+                    Text("Songs")
+                    Text("Genres")
+                    Text("Compilations")
+                    Text("Composers")
+                    Text("Downloaded")
+                }
+                .listStyle(.plain)
+                Text("Recently Added")
+                    .font(.title2.bold())
+            }
+            .navigationTitle("Library")
+        }
     }
 }
 
