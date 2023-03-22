f9affd02027377fb78ae6eb59937afa66742b514
lhy <3200300162@zju.edu.cn>
2023-03-22 20:42:27 +0800

    library

--- FirstProject/Library.swift
+++ FirstProject/Library.swift
@@ -10,40 +10,72 @@ import SwiftUI
 struct Library: View {
     var body: some View {
         NavigationStack {
-            VStack {
-                List {
-                    Text("Playlists")
-                    Text("Artists")
-                    Text("Albums")
-                    Text("Songs")
-                    Text("Genres")
-                    Text("Compilations")
-                    Text("Composers")
-                    Text("Downloaded")
+            List{
+                Section {
+                    NavigationLink {
+                        Playlists()
+                    } label: {
+                        Label("Playlists", systemImage: "music.note.list")
+                    }
+                    NavigationLink {
+                        
+                    } label: {
+                        Label("Artists", systemImage: "music.mic")
+                    }
+                    NavigationLink {
+                        
+                    } label: {
+                        Label("Albums", systemImage: "square.stack")
+                    }
+                    NavigationLink {
+                        
+                    } label: {
+                        Label("Songs", systemImage: "music.note")
+                    }
+                    NavigationLink {
+                        
+                    } label: {
+                        Label("Genres", systemImage: "guitars")
+                    }
+                    NavigationLink {
+                        
+                    } label: {
+                        Label("Compilations", systemImage: "person.2.crop.square.stack")
+                    }
+                    NavigationLink {
+                        
+                    } label: {
+                        Label("Composers", systemImage: "music.quarternote.3")
+                    }
+                    NavigationLink {
+                        
+                    } label: {
+                        Label("Downloaded", systemImage: "arrow.down.circle")
+                    }
                 }
-                .listStyle(.plain)
-                Text("Recently Added")
-                    .font(.title2.bold())
-                    .frame(width: .infinity, height: .infinity, alignment: .topLeading)
-                    .border(.red)
-                HStack {
-                    RoundedRectangle(cornerRadius: 10)
-                        .frame(width: 150, height: 150, alignment: .topLeading)
-                        .padding()
-                    RoundedRectangle(cornerRadius: 10)
-                        .frame(width: 150, height: 150, alignment: .topLeading)
-                        .padding()
-                }
-                HStack {
-                    RoundedRectangle(cornerRadius: 10)
-                        .frame(width: 150, height: 150, alignment: .topLeading)
-                    RoundedRectangle(cornerRadius: 10)
-                        .frame(width: 150, height: 150, alignment: .topLeading)
+                .font(.title2)
+                Section(header: Text("Recently Added")) {
+                    HStack {
+                        RoundedRectangle(cornerRadius: 10)
+                            .frame(width: 150, height: 150, alignment: .topLeading)
+                            .padding()
+                        RoundedRectangle(cornerRadius: 10)
+                            .frame(width: 150, height: 150, alignment: .topLeading)
+                            .padding()
+                    }
+                    HStack {
+                        RoundedRectangle(cornerRadius: 10)
+                            .frame(width: 150, height: 150, alignment: .topLeading)
+                        RoundedRectangle(cornerRadius: 10)
+                            .frame(width: 150, height: 150, alignment: .topLeading)
+                    }
                 }
             }
+            .listStyle(.plain)
             .navigationTitle("Library")
         }
     }
+        
 }
 
 struct Library_Previews: PreviewProvider {
---
+++ FirstProject/Playlists.swift
@@ -0,0 +1,30 @@
+//
+//  Playlists.swift
+//  FirstProject
+//
+//  Created by lhy on 2023/3/22.
+//
+
+import SwiftUI
+
+struct Playlists: View {
+    
+    @State private var searchText: String = ""
+    
+    var body: some View {
+        NavigationStack {
+            List {
+                Text("Playlists 1")
+            }
+            .navigationTitle("Playlists")
+            
+        }
+        .searchable(text: $searchText)
+    }
+}
+
+struct Playlists_Previews: PreviewProvider {
+    static var previews: some View {
+        Playlists()
+    }
+}
